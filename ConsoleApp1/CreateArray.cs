173141f32b0c5eb82bc812856b31ce21548b8276
Lazerneek123 <Lazerneek123@yandex.ru>
2020-10-22 11:50:17 +0300

    масив

---
+++ ConsoleApp1/CreateArray.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProgram
+{
+    class CreateArray
+    {
+        private int[] array;
+        public int size_Array = GetSize(0);
+        static int k = 0;
+
+        static int GetSize(int size_Array)
+        {
+            if (k == 0)    // цей if зайвий але там в коді,(Program.cs а саме Main) я викликаю другий раз клас CreateArray тільки вже з параметром, а саме масивом array, то він потрібний щоб не викликать зайвий раз це тіло блоку (частинку коду)!
+            {
+                size_Array = Int32.Parse(Console.ReadLine());
+                
+            }
+            k++;
+
+            return size_Array;
+        }
+
+        public CreateArray(params int[] array)
+        {
+            this.array = new int[array.Length];
+
+            for (int i = 0; i < array.Length; ++i)
+            {
+                this[i] = array[i];
+            }
+                
+        }
+
+        
+        public int Length
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+        
+        public int this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < Length)
+                {
+                   return array[i];
+                }
+                else
+                {
+                    return 0;
+                }
+                    
+            }
+            set
+            {
+                if (i >= 0 && i < Length && value >= -10 && value <= 10)
+                {
+                    array[i] = value;
+                } 
+            }
+        }
+
+       /* public int Size_Check
+        {
+            get
+            {
+                return size_Array;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    size_Array = value;
+                }
+                else
+                {
+                    size_Array = 2;
+                }
+
+            }
+        }
+
+        public CreateArray(int size_Array)
+        {
+            this.size_Array = size_Array;
+        }*/
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProgram
+{
+    class Program
+    {
+        static int[] Filling_The_Array_KeyBoard(int[] array)
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = Int32.Parse(Console.ReadLine());                
+            }
+
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть розмір одновимірного масива А (тобто від 2 і більше):");
+
+            CreateArray array_A = new CreateArray();
+            int size_Array = array_A.size_Array;
+
+            if (size_Array >= 2)
+            {
+                Console.WriteLine("Введіть в одновимірний масив, під назвою А і який складається з {0} елементів, числа в діапазоні від -10 до 10 (якщо дане вами число не буде входити в цей діапазон, то програма автоматично запише у ваш елемент нуль):", size_Array);
+
+                int[] array = new int[size_Array];
+
+                array = Filling_The_Array_KeyBoard(array);
+
+                array_A = new CreateArray(array);
+
+                int max = 0;
+                int min = 0;
+                max = Find_Maximum_Element(max, array_A);
+                min = Find_Minimum_Element(min, array_A);
+
+                Console.Write("Ваш масив: ");
+
+                for (int i = 0; i < array_A.Length; ++i)
+                {
+                    Console.Write("{0}, ", array_A[i]);
+                }
+
+                Console.Write("(мінімальний елемент в масиві: min = {0}; ", min);
+                Console.Write("максимальний: max = {0})!", max);
+
+            }
+            else
+            {
+                Console.WriteLine("Неправильний розмір, спробуйте ще раз!");
+            }
+
+            Console.ReadKey();
+
+        }
+        
+        static int Find_Maximum_Element(int max, CreateArray array)
+        {
+            max = array[0];
+
+            for (int i = 1; i < array.Length; ++i)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+
+            }
+
+            
+
+            return max;
+        }
+
+        static int Find_Minimum_Element(int min, CreateArray array)
+        {
+            min = array[0];
+
+            for (int i = 1; i < array.Length; ++i)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+                
+            }
+
+            
+
+            return min;
+        }
+
+    }
+}
